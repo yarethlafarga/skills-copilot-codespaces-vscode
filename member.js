801457e4e95f42a772d6e61cc14176fdcc370722
yarethlafarga <97916920+yarethlafarga@users.noreply.github.com>
2024-06-21 06:00:41 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+  return [
+    { id: 1, name: 'HTML', level: 'Advanced' },
+    { id: 2, name: 'CSS', level: 'Intermediate' },
+    { id: 3, name: 'JavaScript', level: 'Beginner' },
+  ];
+}
